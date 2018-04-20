748c956caaa9efb7184a9c067a6c26a3ad7600ff
Pulkit gera <pulkithifi@yahoo.com>
2018-04-20 23:27:43 +0530

    prechanges

--- myApp/app.py
+++ myApp/app.py
@@ -18,14 +18,14 @@ from random import shuffle
 loggedin=False
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:#Neel1998@localhost/NEWS'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:aarush123@@localhost/NEWS'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True 
 db = SQLAlchemy(app)
-news = MySQLdb.connect(host = "localhost",user = "root",passwd = "#Neel1998",db="NEWS")
+news = MySQLdb.connect(host = "localhost",user = "root",passwd = "aarush123@",db="NEWS")
 newsCursor = news.cursor(cursorclass=MySQLdb.cursors.DictCursor)
 app.config['MYSQL_HOST']='localhost'
 app.config['MYSQL_USER']='root'	
-app.config['MYSQL_PASSWORD']='#Neel1998'
+app.config['MYSQL_PASSWORD']='aarush123@'
 app.config['MYSQL_DB']='NEWS'
 app.config['MYSQL_CURSORCLASS']='DictCursor'
 
